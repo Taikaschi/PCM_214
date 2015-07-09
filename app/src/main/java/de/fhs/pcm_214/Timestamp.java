0791ac08d871cf9927d01bcb7eec049cdffcaacf
tim <schieck@stud.fh-sm.de>
2015-07-09 21:23:40 +0200

    ne Menge Methoden hinzugefügt die nötig sind um die ListView ordentlich anzuzeigen....

---
+++ app/src/main/java/de/fhs/pcm_214/Timestamp.java
@@ -0,0 +1,74 @@
+package de.fhs.pcm_214;
+
+import java.util.Calendar;
+
+
+
+/**
+ * Created by soma on 09.07.15.
+ */
+public class Timestamp {
+
+    private Calendar calendar;
+
+    private int day;
+    private int month;
+    private int year;
+
+    public Timestamp() {}
+
+    public Timestamp(int year, int month, int day) {
+        this.day = day;
+        this.month = month;
+        this.year = year;
+    }
+
+    public Timestamp(String yyyymmdd) {
+        this.day = Integer.parseInt(getDay(yyyymmdd));
+        this.month = Integer.parseInt(getMonth(yyyymmdd));
+        this.year = Integer.parseInt(getYear(yyyymmdd));
+    }
+
+    public int getYear() {
+        return year;
+    }
+    public int getMonth() {
+        return month;
+    }
+    public int getDay() {
+        return day;
+    }
+
+    public String getYear(String yyyymmdd) { return yyyymmdd.substring(0,4); }
+    public String getMonth(String yyyymmdd) { return yyyymmdd.substring(4,6); }
+    public String getDay(String yyyymmdd) { return yyyymmdd.substring(6,8); }
+
+    public String getTimestampToday() {
+        calendar = Calendar.getInstance();
+        int today = (calendar.get(Calendar.YEAR) * 10000) + ((calendar.get(Calendar.MONTH) + 1) * 100) +(calendar.get(Calendar.DAY_OF_MONTH));
+        return(String.valueOf(today));
+    }
+
+    // getTimestampWithOffset(-9); --> Heute - 9 Tage
+    // getTimestampWithOffset(17); --> Heute + 17 Tage
+    public String getTimestampWithOffset(int days) {
+        calendar = Calendar.getInstance();
+        calendar.add(Calendar.DAY_OF_MONTH, days);
+        int daywithoffset = (calendar.get(Calendar.YEAR) * 10000) + ((calendar.get(Calendar.MONTH) + 1) * 100) + (calendar.get(Calendar.DAY_OF_MONTH));
+        return(String.valueOf(daywithoffset));
+    }
+
+    public String getNextWeek() {
+        calendar = Calendar.getInstance();
+        calendar.add(Calendar.DAY_OF_MONTH, 7);                                                                                                         // +7
+        int daywithoffset = (calendar.get(Calendar.YEAR) * 10000) + ((calendar.get(Calendar.MONTH) + 1) * 100) + (calendar.get(Calendar.DAY_OF_MONTH));
+        return (String.valueOf(daywithoffset));
+    }
+
+    public String getLastWeek() {
+        calendar = Calendar.getInstance();
+        calendar.add(Calendar.DAY_OF_MONTH, -7);                                                                                                        //-7
+        int daywithoffset = (calendar.get(Calendar.YEAR) * 10000) + ((calendar.get(Calendar.MONTH) + 1) * 100) + (calendar.get(Calendar.DAY_OF_MONTH));
+        return (String.valueOf(daywithoffset));
+    }
+}
